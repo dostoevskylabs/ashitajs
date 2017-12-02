ae2aa1d5c8a37cada75ccddb939f51b36a5ca678
mooglesonthecob <mooglesonthecob@users.noreply.github.com>
2017-12-02 03:00:41 -0500

    cleaning up some client index

--- public/js/index.js
+++ public/js/index.js
@@ -5,57 +5,64 @@
  * @author     dostoevskylabs
  */
 "use strict";
-class Socket{
-  constructor( nodeId ) {
-    this.ws = new WebSocket( "ws://" + nodeId );
-    this.node = window.origin.substr(7);
-    this.onNodeDiscovery = undefined;
 
-    this.ws.addEventListener( "open" , this.onOpen.bind(this) );
-    this.ws.addEventListener( "close" , this.onClose.bind(this) );
-    this.ws.addEventListener( "message" , this.onMessage.bind(this) );
-    this.ws.addEventListener( "error" , this.onError.bind(this) );
+
+
+class AshitaSocket extends WebSocket{
+  constructor(nodeUrl){
+
+    super("ws://" + nodeUrl);
+    this.nodeUrl = nodeUrl;
+    this.addEventListener('open', this.onOpen);
+    this.addEventListener('close', this.onClose);
+    this.addEventListener('message', this.onMessage);
+    this.addEventListener('error', this.onError);
+    
+    this.onReceiveMOTD;
+    this.onPublicMessage;
+    this.onNodeDiscovery;
+
   }
-
-  send( data ) {
-    let payload = JSON.stringify( data );
-    if ( this.ws.readyState === 1 ) {
-      this.ws.send( payload );
-      return true;
+  send(data){
+    console.info("AshitaSocket => send", data);
+    
+    data = JSON.stringify(data);
+    if(this.readyState === this.OPEN){
+      super.send(data);
     }
-    return false;
   }
-
-  onClose ( event ) {
-    console.warn("CLOSING", event);
+  onOpen(event){
+    console.info("Socket => onOpen", event);
+    this.handshake();
   }
-
-  onError ( event ) {
-    console.error(event);
+  onClose(event){
+    console.info("Socket => onClose", event);
   }
+  onMessage(event){
+    console.info("Socket => onMessage", event);
 
-  onMessage ( event ) {
-    let payload = JSON.parse(event.data);
-    if ( this.ws.readyState === 1 ) {
-      switch ( payload.type ) {
+    let data = JSON.parse(event.data);
+
+    if ( this.readyState === this.OPEN ) {
+      switch ( data.type ) {
         case "nodeOwnerConnected":
-          console.log("nodeOwnerConnected Event Received");
+          console.info("nodeOwnerConnected Event Received");
         break;
 
         case "MOTD":
-          this.onReceiveMOTD(payload.content);
+          this.onReceiveMOTD(data.content);
         break;
 
         case "nodeConnected":
-          console.log("nodeConnected Event Received");
+          console.info("nodeConnected Event Received");
         break;
 
         case "nodeDiscovered":
-          this.onNodeDiscovery(payload.content.node);
+          this.onNodeDiscovery(data.content.node);
         break;
 
         case "publicMessage":
-          this.onPublicMessage(payload.content);
+          this.onPublicMessage(data.content);
         break;
 
         default:
@@ -63,48 +70,99 @@ class Socket{
       }
     }
   }
+  onError(event){
+    console.error("Socket => onError", event);
+  }
 
-  onOpen ( event ) {
-    this.send({"handshake":this.node});
+  handshake(){
+    this.send({"handshake":location.host});
   }
 }
 
 
-class Ashita{
-  constructor(ui){
-    this.sockets = {};
+class Ashita {
+  constructor ( ui = undefined ) {
+    this.nodes = new Map();
     this.ui = ui;
-
-    this.ui.onInput = this.onInput.bind(this);
+    this.state = this.addNode(location.host);
+    this.ui.input = this.onUiInput.bind(this);
   }
 
-  onInput ( data ) {
-    this.ui.print({
-      "nodeId"    : this.ui.state,
-      "username"  : data.username,
-      "message"   : data.message
-    });
-    this.sockets[this.ui.state].send({
-      "type":"publicMessage",
-      "content":{
-        "nodeId"   : this.ui.state,
-        "username" : data.username,
-        "message"  : data.message
+  onUiInput(data){
+    if(this.ui){
+      this.ui.print({
+        "nodeId": this.state.nodeUrl,
+        "username": data.username,
+        "message": data.message
+      });
+    }
+
+    this.state.send({
+      type: "publicMessage",
+      content:{
+        nodeId   : this.state.nodeUrl,
+        username : data.username,
+        message  : data.message,
       }
     });
   }
 
-  addSocket (nodeId) {
-    let newSocket = new Socket(nodeId);
-    this.sockets[nodeId] = newSocket;
-    newSocket.onNodeDiscovery = this.onNodeDiscovery.bind(this);
-    newSocket.onReceiveMOTD = this.onReceiveMOTD.bind(this);
-    newSocket.onPublicMessage = this.onPublicMessage.bind(this);
+  addNode ( nodeUrl ) {
+    let node = new AshitaSocket(nodeUrl);
+    node.onReceiveMOTD = this.onReceiveMOTD.bind(this);
+    node.onPublicMessage = this.onPublicMessage.bind(this);
+    node.onNodeDiscovery = this.onNodeDiscovery.bind(this);
+    this.nodes.set(node.nodeUrl, node);
 
-    // hack
-    const menu = document.getElementById("menu");
-    const parts = nodeId.split(":");
-    this.ui.state = nodeId; //selected channel
+    if(this.ui){
+      this.ui.addNode(node);
+    }
+
+    return node;
+  }
+
+  onReceiveMOTD ( data ) {
+    console.log("onReceiveMOTD", this);
+    if(this.ui){
+      this.ui.print( {
+        type   : "blank",
+        nodeId : data.nodeId,
+        message: data.MOTD
+      });
+    }
+  }
+
+  onPublicMessage ( data ) {
+    if(this.ui){
+      this.ui.print( { username: data.username, message: data.message } );
+    }
+  }
+
+  onNodeDiscovery ( nodeUrl ) {
+    if ( this.nodes.get(nodeUrl) ) {
+      return
+    }
+    this.addNode(nodeUrl);
+    if(this.ui){
+      this.ui.print( { type:"notice", message:"New peer discovered: " + nodeUrl } );
+    }
+
+  }
+
+}
+
+class UI {
+  constructor () {
+    this.input = document.getElementById("input");
+    this.output = document.getElementById("output");
+    this.menu = document.getElementById("menu");
+
+    this.input = noop => {};
+    input.addEventListener( 'keydown', this.inputKeydown.bind(this) );
+  }
+
+  addNode(node){
+    const parts = node.nodeUrl.split(":");
     const newEntry = `<div class="node">
       <img class="icon" src="./assets/node.svg"/>
       <div class="address">${parts[0]}
@@ -112,40 +170,6 @@ class Ashita{
       </div>
     </div>`;
     menu.insertAdjacentHTML('beforeend', newEntry);
-  }
-
-  onReceiveMOTD( data ){
-    this.ui.print({
-      "type":"blank",
-      "nodeId":data.nodeId,
-      "message":data.MOTD
-    });
-  }
-
-  onNodeDiscovery(nodeId){
-    if ( Object.keys(this.sockets).indexOf(nodeId) !== -1 )
-      return;
-    this.ui.print({
-      type:"notice",
-      message:"New peer discovered: " + nodeId
-    });
-    this.addSocket(nodeId);
-  }
-
-  onPublicMessage(data){
-    this.ui.print({
-      username:data.username,
-      message:data.message
-    });
-  }
-}
-
-class UI {
-  constructor () {
-    this.input = document.getElementById("input");
-    this.output = document.getElementById("output");
-    this.onInput = undefined;
-    input.addEventListener( 'keydown', this.inputKeydown.bind(this) );
   }
 
   inputKeydown ( event ) {
@@ -156,26 +180,21 @@ class UI {
       };
       event.target.value = "";
 
-      if(this.onInput)
-        this.onInput( data );
+      this.input( data );
     }
   }
 
   createEntry ( data ) {
+    let entry;
+
     const time = new Intl.DateTimeFormat('en-US', {
       hour: 'numeric', minute: 'numeric', second: 'numeric'
     }).format(data.timestamp);
-    if ( data.type === "blank" ) {
-      const entry = `
-      <div class="entry blank">
-        <div class="msg">${data.message}</div>
-      </div>
-      `;
-      return entry;      
-    }
 
     if(data.hasOwnProperty("type")){
       switch(data.type){
+        case "blank":
+        break;
         case "error":
           data.username = "\uf06a";
         break;
@@ -190,13 +209,21 @@ class UI {
           return;
       }
     }
-    const entry = `
-    <div class="entry ${data.type ? data.type : ''}"">
-      <time class="date" datetime="${data.timestamp}">${time}</time>
-      <div class="user ${data.type ? 'fa' : ''}">${data.username}</div>
-      <div class="msg">${data.message}</div>
-    </div>
-    `;
+    
+    if(data.type === "blank"){
+      entry = `
+      <div class="entry blank">
+        <div class="msg">${data.message}</div>
+      </div>`;
+    }else{
+      entry = `
+      <div class="entry ${data.type ? data.type : ''}"">
+        <time class="date" datetime="${data.timestamp}">${time}</time>
+        <div class="user ${data.type ? 'fa' : ''}">${data.username}</div>
+        <div class="msg">${data.message}</div>
+      </div>`;
+    }
+
     return entry;
   }
 
@@ -214,4 +241,3 @@ class UI {
 
 let ui = new UI();
 let ashita = new Ashita(ui);
-ashita.addSocket(window.origin.substr(7))
