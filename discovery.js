bf235f1ae85d12db11942c8633eb61d30c70c84b
Elijah <dostoevskylabs@users.noreply.github.com>
2019-01-08 14:58:36 -0500

    Update discovery.js

--- discovery.js
+++ discovery.js
@@ -2,55 +2,55 @@ const nodeManager       = require("./middleware/peerManager");
 const cli               = require("./lib/ui");
 
 function discoverPeers( repeat ) {
-  var mdns = require('mdns');
-  var ad = mdns.createAdvertisement(mdns.tcp('ashitajs'), nodeManager.getNodePort);
-  ad.start();
-   
-  // watch all http servers
-  var browser = mdns.createBrowser(mdns.tcp('ashitajs'));
-  browser.on('serviceUp', function(service) {
-    try {
-        /**
-         * This is done to grab the IPv4 address from mdns information
-         * different platforms and systems store it in different indexes
-         * so we recurively go through them looking for an IPv4 address to use.
-         * furthermore we could restrict this to ip ranges we want to interact with
-         */
-        let nodeHost = (function getIPv4 ( ind ){
-          if ( service.addresses[ind].includes(':') ) return getIPv4( ind + 1 );
-          return service.addresses[ind];
-        })( 0 );
+  try {
+    var mdns = require('mdns');
+    var ad = mdns.createAdvertisement(mdns.tcp('ashitajs'), nodeManager.getNodePort);
+    ad.start();
 
-        if ( !nodeHost ) return false; // no ipv4 discovered in the broadcast
-        if ( nodeManager.getActivePeers.includes(`${nodeHost}:${service.port}`) ) return false; // if we already know this peer
-        if ( nodeHost === '127.0.0.1' && service.port === nodeManager.getNodePort ) return false; // connecting to ourselves lol
-        // if it matches ourself, we shouldn't connect, lol
-        if ( nodeHost === nodeManager.getNodeHost && service.port === nodeManager.getNodePort ) {
+    // watch all http servers
+    var browser = mdns.createBrowser(mdns.tcp('ashitajs'));
+    browser.on('serviceUp', function(service) {
+          /**
+           * This is done to grab the IPv4 address from mdns information
+           * different platforms and systems store it in different indexes
+           * so we recurively go through them looking for an IPv4 address to use.
+           * furthermore we could restrict this to ip ranges we want to interact with
+           */
+          let nodeHost = (function getIPv4 ( ind ){
+            if ( service.addresses[ind].includes(':') ) return getIPv4( ind + 1 );
+            return service.addresses[ind];
+          })( 0 );
 
-          return false;
-        } else {
+          if ( !nodeHost ) return false; // no ipv4 discovered in the broadcast
+          if ( nodeManager.getActivePeers.includes(`${nodeHost}:${service.port}`) ) return false; // if we already know this peer
+          if ( nodeHost === '127.0.0.1' && service.port === nodeManager.getNodePort ) return false; // connecting to ourselves lol
+          // if it matches ourself, we shouldn't connect, lol
+          if ( nodeHost === nodeManager.getNodeHost && service.port === nodeManager.getNodePort ) {
 
-          // in this case we are connecting, debug information logged:
-          cli.Panel.debug('Discovered node: ' + nodeHost + ':' + service.port);
-          nodeManager.connectToPeer( nodeHost, service.port );
-        }
-    } catch ( e ) {
-      cli.Panel.debug(e);
-    }
-  });
+            return false;
+          } else {
 
-  browser.on('serviceDown', function(service) {
-    // we aren't handling this yet
-    try {
-      //nodeManager.removeNode( nodeManager.generatePeerId(`${service.addresses[1]}:${service.port}`) );
-    } catch ( e ) {}
-  });
-  browser.start(); // start-o!
+            // in this case we are connecting, debug information logged:
+            cli.Panel.debug('Discovered node: ' + nodeHost + ':' + service.port);
+            nodeManager.connectToPeer( nodeHost, service.port );
+          }
+    });
 
-  setTimeout(function(){
-    cli.screens["Log"].setLabel("Log"); // hack-o!
-  }, 3000);
-  return repeat(); // repeat-o!
+    browser.on('serviceDown', function(service) {
+      // we aren't handling this yet
+      try {
+        //nodeManager.removeNode( nodeManager.generatePeerId(`${service.addresses[1]}:${service.port}`) );
+      } catch ( e ) {}
+    });
+    browser.start(); // start-o!
+
+    setTimeout(function(){
+      cli.screens["Log"].setLabel("Log"); // hack-o!
+    }, 3000);
+    return repeat(); // repeat-o!
+  } catch ( e ) {
+    cli.Panel.debug(e);
+  }
 }
 
 (function repeat(){
