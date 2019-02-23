c7f7825598282697836cc47f278d674d0261e8ae
Fyodor Dostoevsky <dostoevsky@redspy.local>
2019-02-22 22:55:40 -0500

    a

--- discovery.js
+++ discovery.js
@@ -11,7 +11,7 @@ if ( v1 ) {
   mdns.createAdvertisement(mdns.tcp('ashitajs'), nodeManager.getNodePort, {networkInterface: nodeManager.getInterface}).start();
 } else {
   bonjour           = require('bonjour')({multicast:true});
-  let broadcast = bonjour.publish({ name: `hostname`, type: 'ashitajs', port: nodeManager.getNodePort });
+  let broadcast = bonjour.publish({ name: `hostname-${require('shortid').generate()}`, type: 'ashitajs', port: nodeManager.getNodePort });
   broadcast.on('up', function(){
     cli.Panel.debug("Broadcasting: ", nodeManager.getNodePort);
   });  
